e2e218a29a1590777c4f64e962ee746842fddba2
MYBV <yusseth12@gmail.com>
2023-03-08 23:44:00 -0500

    types faltantes y async

--- index.ts
+++ index.ts
@@ -9,17 +9,17 @@ const app = express();
 
 //################################################################################
 function fibonacci(num: number): number {
-    let raiz_5 = Math.sqrt(5);
-    let raiz01 = (1 + raiz_5) / 2;
-    let raiz02 = (1 - raiz_5) / 2;
+    let raiz_5: number = Math.sqrt(5);
+    let raiz01: number = (1 + raiz_5) / 2;
+    let raiz02: number = (1 - raiz_5) / 2;
 
     return Math.round((Math.pow(raiz01, num) - Math.pow(raiz02, num)) / raiz_5);
 }
 //################################################################################
 
 //################################################################################
-app.get("/fibonacci/:numero([0-9]+)", (req, res) => {
-    let valida_numero = ValidatorNum(req.params.numero);
+app.get("/fibonacci/:numero([0-9]+)", async(req, res) => {
+    let valida_numero: Boolean = ValidatorNum(req.params.numero);
     if (!valida_numero)
         return res
             .status(400)
@@ -28,8 +28,8 @@ app.get("/fibonacci/:numero([0-9]+)", (req, res) => {
                     "El indice a requerir debe ser numerico mayor o igual a cero",
             });
 
-    let numero = parseInt(req.params.numero, 10);
-    let resultado = fibonacci(numero);
+    let numero: number = parseInt(req.params.numero);
+    let resultado: number = await fibonacci(numero);
 
     res.status(200).json({ indice: req.params.numero, valor: resultado });
 });
